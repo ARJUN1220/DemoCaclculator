25327ce8fd376d647ee589af65f4a8e8ded9e368
ARJUN1220 <arjungangani12@gmail.com>
2024-02-16 11:03:13 +0530

    created project

---
+++ src/main/java/com/example/spe_calculator/SpeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.spe_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spe_calculator/SpeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spe_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
